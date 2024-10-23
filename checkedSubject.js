41bea4fc40ee75eb89730dd6dd49273faf2fd27e
supaw <supawitchtt@gmail.com>
2024-10-23 23:29:52 +0700

    gg

--- checkedSubject.js
+++ checkedSubject.js
@@ -7,12 +7,17 @@ function checkedSubject() {
 
     for (let i = 0; i < divs.length; i++) {
         const div = divs[i];
+        console.log("Processing div:", div);
                 // สุ่มเลือก value ระหว่าง 3 ถึง 5
         const randomValue = getRandomInt(3, 5);
+        console.log("Random Value:", randomValue);
                 // เลือก input ที่มีค่า value ตรงกับที่สุ่มได้
         const randomChoice = div.querySelector(input[type="radio"][value="${randomValue}"]);
+        console.log("Random Choice found:", randomChoice);
+        
         if (randomChoice) {
-            randomChoice.checked = true; // ติ๊กตัวเลือกที่สุ่มได้
+            randomChoice.checked = true;
+            console.log("Checked:", randomChoice); // ติ๊กตัวเลือกที่สุ่มได้
         }
     }
 }
