3d0a83fd172b9c4dd3eaff9f70b5498ea859ba18
supaw <supawitchtt@gmail.com>
2024-10-23 23:20:16 +0700

    change

--- checkedSubject.js
+++ checkedSubject.js
@@ -1,10 +1,14 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function checkedSubject() {
     const divs = document.querySelectorAll('div[style="margin-left:35px; margin-bottom:10px;"]');
 
     for (let i = 0; i < divs.length; i++) {
         const div = divs[i];
                 // สุ่มเลือก value ระหว่าง 3 ถึง 5
-        const randomValue = Math.floor(Math.random() * (3)) + 3;
+        const randomValue = getRandomInt(3, 5);
                 // เลือก input ที่มีค่า value ตรงกับที่สุ่มได้
         const randomChoice = div.querySelector(input[type="radio"][value="${randomValue}"]);
         if (randomChoice) {
